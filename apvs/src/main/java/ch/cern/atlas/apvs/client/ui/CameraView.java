96d7eb18be2a20b54c3ba9231cf5419f93a83b34
Eleni <eleni_adamidi@iasa.gr>
2015-01-29 16:06:38 +0200

    edusafe changes

--- apvs/src/main/java/ch/cern/atlas/apvs/client/ui/CameraView.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/ui/CameraView.java
@@ -88,7 +88,7 @@ public class CameraView extends ImageView implements Module,
 		SwitchWidgetEvent.register(switchBus, new SwitchWidgetEventHandler(
 				switchBus, element, this));
 
-		init(clientFactory, "100%", "100%");
+		init(clientFactory, "50%", "50%");
 
 		return true;
 	}
@@ -169,6 +169,9 @@ public class CameraView extends ImageView implements Module,
 //			@Override
 //			public void execute() {
 				setUrl(cameraUrl);
+				setWidth("640");
+				setHeight("480");
+				
 //				Bootbox.alert("CameraURL: '"+ptuId+"' '"+cameraUrl+"'");
 //			}
 //		});
